1fe2938fb0a8606abd1c10cf97f991aeaa4a05e0
Manuel Maturano <mmaturano.epidemio@gmail.com>
2026-01-13 16:02:31 -0300

    1

--- storms.R
+++ storms.R
@@ -46,7 +46,10 @@ if(!file.exists("FStormData.csv")){
                 destfile = file.path(getwd(), "FStormData.csv"))
 }
 
-storms <- fread("FStormData.csv", na.strings = "") |> janitor::clean_names()
+storms <- fread("FStormData.csv", 
+                na.strings = "", 
+                showProgress = TRUE, 
+                verbose = TRUE) |> janitor::clean_names()
 
 # ---- Data cleaning ----
 dates <- names(storms)[names(storms) %like% "date"] 
@@ -70,8 +73,8 @@ tidy_storms <- tidy_storms[, .(bgn_date, end_date, state = as.factor(state),
                            injuries, propdmg, propdmgexp, cropdmg, cropdmgexp)]
 
 # Acording to NOAA, the types of events are:
-# Astronomical Low Tide, Avalanche, Blizzard, Coastal Flood,
-# Cold/Wind Chill, Debris Flow, Dense Fog, Dense Smoke, Drought,
+# Astronomical High Tide, Astronomical Low Tide, Avalanche, Blizzard, 
+# Coastal Flood, Cold/Wind Chill, Debris Flow, Dense Fog, Dense Smoke, Drought,
 # Dust Devil, Dust Storm, Excessive Heat, 
 # Extreme Cold/Wind Chill, Flash Flood, Flood, Freezing Fog,
 # Frost/Freeze, Funnel Cloud, Hail, Heat, Heavy Rain, Heavy Snow,
@@ -81,11 +84,68 @@ tidy_storms <- tidy_storms[, .(bgn_date, end_date, state = as.factor(state),
 # Rip Current, Seiche, Sleet, Storm Tidem, Strong Wind, Thunderstorm Wind,
 # Tornado, Tropical Depression, Tropical Storm, Tsunami, 
 # Volcanic Ash, Waterspout, Wildfire, Winter Storm, Winter Weather
-tidy_storms[, evtype := stringr::str_to_title(evtype)]
-tidy_storms[, unique(evtype)] |> sample(20)
-tidy_storms[, clean_event := fcase(evtype %like% "Tornado", Tornado,
-                                   default = evtype)]
 
+tidy_storms[, evtype := stringr::str_to_title(evtype)]
+tidy_storms[, unique(evtype)] |> sort()
+tidy_storms <- tidy_storms[!clean_event %like% "(?i)Summary"]
+tidy_storms[, clean_event := fcase(
+  evtype %like% "(?i)Flash Flood", "Flash Flood", # 
+  evtype %like% "(?i)Light|Ligh?tning|Ligntning", "Lightning", #
+  evtype %like% "(?i) Devil|Devel", "Dust Devil", #
+  evtype %like% "(?i)Dust Storm", "Dust Storm", #
+  evtype %like% "(?i)Lakeshore Flood", "Lakeshore Flood", # 
+  evtype %like% "(?i)Funnel Cloud", "Funnel Cloud", #
+  evtype %like% "(?i)Coastal Flood|Cstl Flood|Tidal Flood", "Coastal Flood", #
+  evtype %like% "(?i)Lake Effect Snow", "Lake-Effect Snow", #
+  evtype %like% "(?i)Marine (Thunderstorm|Tstm)", "Marine Thunderstorm Wind", #
+  evtype %like% "(?i)Winter Storm", "Winter Storm", #
+  evtype %like% "(?i)Winter Weather", "Winter Weather", #
+  evtype %like% "(?i)Tornado|Torn?da?o|Gustnado|Landspout", "Tornado", # 
+  evtype %like% "(?i)Ice Storm", "Ice Storm", #
+  evtype %like% "(?i)Thunderstorm|Tstm|Storm Force|Burst|Thund?e?e?r?sto?r?m", "Thunderstorm Wind",
+  evtype %like% "(?i)Flood|Fld|Stream", "Flood", # 
+  evtype %like% "(?i)Surf|High Tide|High Water|Swells|Waves", "High Surf", # 
+  evtype %like% "(?i)Hurricane|Typhoon", "Hurricane/Typhoon", #
+  evtype %like% "(?i)Marine Hail", "Marine Hail", # 
+  evtype %like% "(?i)Hail", "Hail", #
+  evtype %like% "(?i)Excessive Heat", "Excessive Heat", #
+  evtype %like% "(?i)Heat|Warm|Hot", "Heat", # 
+  evtype %like% "(?i)Extreme Cold|Extreme Wind Chill", "Extreme Cold/Wind Chill", # 
+  evtype %like% "(?i)Cold|Wind Chill|Hypothermia", "Cold/Wind Chill", #
+  evtype %like% "(?i)Tropical Storm", "Tropical Storm", #
+  evtype %like% "(?i)Tropical Depression", "Tropical Depression", #
+  evtype %like% "(?i)Tsunami", "Tsunami", #
+  evtype %like% "(?i)Drought", "Drought", #
+  evtype %like% "(?i)Smoke", "Dense Smoke", #
+  evtype %like% "(?i)Avalanche", "Avalanche", #
+  evtype %like% "(?i)Debris", "Debris Flow", #
+  evtype %like% "(?i)Current", "Rip Current",
+  evtype %like% "(?i)Astronomical High", "Astronomical High Tide",
+  evtype %like% "(?i)Astronomical Low", "Astronomical Low Tide",
+  evtype %like% "(?i)Seiche", "Seiche",
+  evtype %like% "(?i)Sleet", "Sleet",
+  evtype %like% "(?i)Sleet", "Sleet",
+  evtype %like% "(?i)Tidem", "Storm Tidem",
+  evtype %like% "(?i)Volcanic Ash", "Volcanic Ash",# 
+  evtype %like% "(?i)High Wind", "High Wind", #
+  evtype %like% "(?i)Marine Strong Wind", "Marine Strong Wind", #
+  evtype %like% "(?i)Marine High Wind", "Marine High Wind", #
+  evtype %like% "(?i)Strong Wind|Gusty", "Strong Wind",
+  evtype %like% "(?i)Snow", "Heavy Snow", #
+  evtype %like% "(?i)Rain|Shower|Precip", "Heavy Rain", #
+  evtype %like% "(?i)Wildfire|Fire|Forest Fire", "Wildfire", #
+  evtype %like% "(?i)Freezing Fog", "Freezing Fog", #
+  evtype %like% "(?i)Fog", "Dense Fog", #
+  evtype %like% "(?i)Waterspout", "Waterspout", #
+  evtype %like% "(?i)Blizzard", "Blizzard", #
+  evtype %like% "(?i)Frost|Freeze", "Frost/Freeze", #
+    default = evtype
+)]
+tidy_storms[, unique(clean_event)]
+check_impacto <- tidy_storms[clean_event == evtype, 
+                             .(Fatalidades = sum(fatalities), N = .N), 
+                             by = evtype][order(-Fatalidades)]
+head(check_impacto, 50)
 # ---- Human loses ----
 tidy_storms[, .(fatalities, injuries), evtype]
 
